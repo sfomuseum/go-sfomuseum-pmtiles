525b8908d413feb9e0783536a84169d7ed5169b2
whosonfirst <whosonfirst@localhost>
2022-10-11 14:17:23 -0700

    snapshot: work through naming conventions loading tiles

--- example/www/index.js
+++ example/www/index.js
@@ -1,9 +1,11 @@
 window.addEventListener('load', function(e){
 
     const map = L.map("map");
-
     map.setView([37.6143, -122.3828], 13);
+
+    // note that it is 'sfo' and not 'sfo.pmtiles'
+    // this is important
     
-    var layer = protomaps.leafletLayer({url:"/sfo.pmtiles/{z}/{x}/{y}.pbf"});
+    var layer = protomaps.leafletLayer({url:"/sfo/{z}/{x}/{y}.mvt"});
     layer.addTo(map);
 });
