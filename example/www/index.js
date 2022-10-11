aa63b10aef319620293ffb509de2da2bd5bef331
whosonfirst <whosonfirst@localhost>
2022-10-11 15:19:24 -0700

    improved comments

--- example/www/index.js
+++ example/www/index.js
@@ -1,12 +1,14 @@
 window.addEventListener('load', function(e){
 
-    var db_name = document.body.getAttribute("data-example-database");
-    var db_url = "/" + db_name + "/{z}/{x}/{y}.mvt";
-    
+    // Remember: These get assigned at runtime in application/server/app.go
+
+    var db_name = document.body.getAttribute("data-example-database");    
     var str_lat = document.body.getAttribute("data-example-latitude");
     var str_lon = document.body.getAttribute("data-example-longitude");
     var str_zoom = document.body.getAttribute("data-example-zoom");    
 
+    var db_url = "/" + db_name + "/{z}/{x}/{y}.mvt";
+    
     var lat = parseFloat(str_lat);
     var lon = parseFloat(str_lon);
     var zoom = parseInt(str_zoom);        
